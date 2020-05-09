d8dd9897585ae53d5f02baaf0f32424cdda2baeb
sergei_vydra <vydrasergei@gmail.com>
2020-05-09 13:17:58 +0300

    formatted by PEP8, updated wait-sleep time to 30 sec

--- test_items.py
+++ test_items.py
@@ -1,9 +1,11 @@
 from time import sleep
-link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
 
 def test_goods_has_add_to_basket_button(browser):
     browser.get(link)
-    # wait correct loading
-    sleep(10)
+    # wait for correct loading
+    sleep(30)
     add_to_basket = browser.find_element_by_css_selector('.btn-add-to-basket')
     assert add_to_basket, 'button "add item" is absent'
